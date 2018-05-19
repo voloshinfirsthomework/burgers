d41db94ed9ea8882f6e79ed1c454f0c614e26524
Dmitry Voloshin <ermolaevav_85@mail.ru>
2018-05-19 18:30:44 +0300

    gamburger menu, over

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-var logo = document.querySelector('.header__img');
+const menuList = document.querySelector('.header__menu-list');
+const gmBtn = document.querySelector('.header__gamburger-menu');
+const hidden = document.querySelector('.gm');
+var wraper = document.querySelector('.wraper');
+const close = document.querySelector('.gm__close');
+var footer = document.querySelector('.footer');
 
-logo.addEventListener('click', function () {
-  console.log('фаил подключен');
+gmBtn.addEventListener('click', function(){
+  hidden.style.height = '100%';
+  wraper.style.height = '100%';
+  footer.style.height = '0';
+  footer.style.overflow = 'hidden';
+
+  close.addEventListener('click', function(){
+    hidden.style.height = '0';
+    wraper.style.height = 'auto';
+    footer.style.height = 'auto';
+    footer.style.overflow = 'visible';
+  });
 });
