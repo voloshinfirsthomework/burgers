809f4dc2254997308e363e5c895edd4a47cb5629
Dmitry Voloshin <ermolaevav_85@mail.ru>
2018-05-29 18:33:24 +0300

    dla ani

--- js/main.js
+++ js/main.js
@@ -5,16 +5,30 @@ var wraper = document.querySelector('.wraper');
 const close = document.querySelector('.gm__close');
 var footer = document.querySelector('.footer');
 
-gmBtn.addEventListener('click', function(){
+gmBtn.addEventListener('click', function () {
   hidden.style.height = '100%';
   wraper.style.height = '100%';
   footer.style.height = '0';
   footer.style.display = 'none';
 
-  close.addEventListener('click', function(){
+  close.addEventListener('click', function () {
     hidden.style.height = '0';
     wraper.style.height = 'auto';
     footer.style.height = 'auto';
     footer.style.display = 'flex';
   });
 });
+
+var teamItem = document.querySelector('.team__item');
+var teamName = document.querySelector('.team__h3-name');
+var $item = document.getElementsByClassName('team__item');
+var $teamName = document.getElementsByClassName('team__h3-name');
+qw = 'team__item-active';
+
+for (i = 0; i < $item.length; i++) {
+  teamName.addEventListener('click', function (e) {
+    e.preventDefault();
+    // $item[i].classList.remove('team__item');
+    $item[i].classList.add(qw);
+  });
+};
